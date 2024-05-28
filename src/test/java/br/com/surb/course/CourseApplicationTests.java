1df7e289d4d5a6439723d9a708078696580d9564
Claudio Cardoso <claudio.c.lima@hotmail.com>
2024-05-28 09:17:29 -0300

    Initial commit

---
+++ src/test/java/br/com/surb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.surb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
